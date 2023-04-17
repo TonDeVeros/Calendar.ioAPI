8b9dbee0b24273b58df3611a4939ea19c8672ef2
Ton DeVeros <TonDeveros@gmail.com>
2023-04-17 18:29:10 -0300

    Add project files.

---
+++ Calendar.ioWebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using Calendar.ioWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+
+namespace Calendar.ioWebAPI.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //instanciando uma variavel do tipo listaDeClientes para fazer a lista em memória
+        private static List<Customers> clientes = new List<Customers>();
+        private static int idCliente = 0;
+
+
+        [HttpPost]
+        public IActionResult AdcionarCliente([FromBody] Customers cliente)
+        {
+            cliente.Id = idCliente++;
+            clientes.Add(cliente);//adcionando um cliente a listaDeClientes
+            return CreatedAtAction(nameof(SelecionarClientePorId), new { id = cliente.Id }, cliente);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult SelecionarClientePorId(int id)// o retorno de IActionResult é o retorno de uma ação 
+        {
+            var cliente = clientes.FirstOrDefault(cliente => cliente.Id == id);//retorna o cliente com o mesmo id passado no parametro
+            if (cliente == null) return NotFound();
+            return Ok(cliente);
+        }
+    }
+}
---
+++ Calendar.ioWebAPI/Models/Customers.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calendar.ioWebAPI.Models
+{
+    public class Customers
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do cliente é obrigatório!")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "O número do cliente é obrigatório!")]
+        public int CustomerNumber { get; set; }
+
+        [Required(ErrorMessage = "O endereço do cliente é obrigatório!")]
+        public string CustomerAdress { get; set; }
+
+        [Required(ErrorMessage = "A data em que será realizada a visita ao cliente é obrigatória!")]
+        public DateTime ServiceDate { get; set; }
+    }
+}
